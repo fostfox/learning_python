3d77a1d574ad449bf2287c15ddef7beac80e7075
nikbush <kolia.kun@gmail.com>
2024-08-06 21:31:43 +0300

    first test commit

--- task2/app.py
+++ task2/app.py
@@ -3,12 +3,13 @@
 # 
 
 class PhoneBook:
+    book_list = []
+    
     def __init__(self) -> None:
         pass
 
-    def add(self, name: str, surname: str, phone: str) -> None:
-        """??? (https://peps.python.org/pep-0257/)"""
-        pass
+    def add(self, name: str) -> None:
+        self.book_list.append(name)
 
     def update(self, name: str, surname: str, phone: str) -> bool:
         """??? (https://peps.python.org/pep-0257/)"""
@@ -21,6 +22,13 @@ class PhoneBook:
     def search(self, name: str = None, surname: str = None) -> list[dict]:
         """??? (https://peps.python.org/pep-0257/)"""
         return []
+
+
+PhoneBook_1 = PhoneBook()
+PhoneBook_1.add('Bob')
+PhoneBook_1.add('Jon')
+PhoneBook_1.add('Jack')
+print(PhoneBook_1.book_list)
 
 
 # 
