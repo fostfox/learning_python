87ee86e02e8e297bb169749dcf95fae51ccf78db
nikbush <kolia.kun@gmail.com>
2024-09-26 15:37:27 +0300

    added global variables and improved program

--- task4/console_program.py
+++ task4/console_program.py
@@ -1,79 +1,77 @@
-import random, keyboard
+import random
 
-print("\nWelcome to the Fun Console Program!")
+DAD_JOKES_LIST = [
+        "Why don't skeletons fight each other? Because they don't have the guts!",
+        "I was wondering why the baseball was getting bigger. Then it hit me.",
+        "I'm reading a book about anti-gravity. I can't put it down.",
+        "My dolphin puns are terrible on porpoise.",
+        "Orion’s Belt is a huge waist of space."
+    ]
+
+FORTUNE_COOKIES = [
+        "Be at peace with yourself.",
+        "Your home will be filled with peace and harmony.",
+        "Don't hesitate to tackle a difficult problem.",
+        "Get ready for a life-changing event!",
+        "Work with your destiny. Stop trying to outrun it."
+    ]
+
+MAGIC_BALL = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy, try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
+    ]
 
 def wait_for_user():
-    input("\nPress any key to return to the menu...")
-    keyboard.read_event()
+    input("\nPress ENTER to return to the menu...")
 
 def random_message(messages):
-    print(random.choice(messages))
+    print("\n", random.choice(messages), end="\n\n")
     wait_for_user()
 
-def dad_joke_generator():
-    return [
-        "\nWhy don't skeletons fight each other? Because they don't have the guts!",
-        "\nI was wondering why the baseball was getting bigger. Then it hit me.",
-        "\nI'm reading a book about anti-gravity. I can't put it down.",
-        "\nMy dolphin puns are terrible on porpoise.",
-        "\nOrion’s Belt is a huge waist of space."
-    ]
-    
-def fortune_cookie():
-    return [
-        "\nBe at peace with yourself.",
-        "\nYour home will be filled with peace and harmony.",
-        "\nDon't hesitate to tackle a difficult problem.",
-        "\nGet ready for a life-changing event!",
-        "\nWork with your destiny. Stop trying to outrun it."
-    ]
-    
-def magic_ball():
-    return [
-        "\nIt is certain",
-        "\nIt is decidedly so",
-        "\nWithout a doubt",
-        "\nYes definitely",
-        "\nYou may rely on it",
-        "\nAs I see it, yes",
-        "\nMost likely",
-        "\nOutlook good",
-        "\nYes",
-        "\nSigns point to yes",
-        "\nReply hazy, try again",
-        "\nAsk again later",
-        "\nBetter not tell you now",
-        "\nCannot predict now",
-        "\nConcentrate and ask again",
-        "\nDon't count on it",
-        "\nMy reply is no",
-        "\nMy sources say no",
-        "\nOutlook not so good",
-        "\nVery doubtful"
-    ]
-    
 def cli():
-    while True:
-        print("\nPlease choose an option:\n")
-        print("1. Dad Joke Generator")
-        print("2. Fortune Cookie")
-        print("3. Magic 8-Ball")
-        print("4. Exit\n")
+    try:
+        print("\nWelcome to the Fun Console Program!")
+        while True:
+            print("\nPlease choose an option:\n")
+            print("1. Dad Joke Generator")
+            print("2. Fortune Cookie")
+            print("3. Magic 8-Ball")
+            print("4. Exit\n")
 
-        choice = input("Enter your choice (1-4): ")
+            choice = input("Enter your choice (1-4): ")
 
-        if choice == "1":
-            random_message(dad_joke_generator())
-        elif choice == "2":
-            random_message(fortune_cookie())
-        elif choice == "3":
-            input("\nAsk a yes/no question: \n")
-            random_message(magic_ball())
-        elif choice == "4":
-            print("\nGoodbuy! Thank you!\n")
-            break
-        else:
-            print("\nInvalid choice, please select again.")
+            if choice == "1":
+                random_message(DAD_JOKES_LIST)
+            elif choice == "2":
+                random_message(FORTUNE_COOKIES)
+            elif choice == "3":
+                input("\nAsk a yes/no question: \n")
+                random_message(MAGIC_BALL)
+            elif choice == "4":
+                print("\nGoodbuy! Thank you!\n")
+                break
+            else:
+                print("\nInvalid choice, please select again.")
+    except KeyboardInterrupt:
+        print("\n\nThe program was terminated by the user (Ctrl+C).\n")
 
 if __name__ == "__main__":
     cli()
