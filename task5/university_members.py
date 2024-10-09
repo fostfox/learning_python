81b777508bfcfcb643b01817c93f923561da5518
nikbush <kolia.kun@gmail.com>
2024-10-09 19:03:05 +0300

    Fixed comments

--- task5/university_members.py
+++ task5/university_members.py
@@ -3,12 +3,12 @@ import pickle
 import os
 from colors import RED, GREEN, YELLOW, BLUE, RESET
 
-FILE = "university_members_file"
+FILE_NAME = "university_members_file"
 
 class UniversityMember:
     """"Basic class for university members."""
 
-    def __init__(self, id: int, name: str, age: str) -> None:
+    def __init__(self, id: int, name: str, age: int) -> None:
         self.id = id
         self.name = name
         self.age = age
@@ -17,7 +17,7 @@ class UniversityMember:
 class Student(UniversityMember):
     """The class representing the student."""
 
-    def __init__(self, id: int, name: str, age: str, course: str, year: int, gpa: str) -> None:
+    def __init__(self, id: int, name: str, age: int, course: int, year: int, gpa: float) -> None:
         super().__init__(id, name, age)
         self.course = course
         self.year = year
@@ -27,7 +27,7 @@ class Student(UniversityMember):
 class Professor(UniversityMember):
     """The class representing the professors."""
 
-    def __init__(self, id, name, age, department, title, salary):
+    def __init__(self, id: int, name: str, age: int, department: str, title: str, salary: float):
         super().__init__(id, name, age)
         self.department = department
         self.title = title
@@ -37,7 +37,7 @@ class Professor(UniversityMember):
 class AdminStaff(UniversityMember):
     """The class representing the admin_staffs."""
 
-    def __init__(self, id, name, age, position, years_of_service, salary):
+    def __init__(self, id: int, name: str, age: int, position: str, years_of_service: int, salary: float):
         super().__init__(id, name, age)
         self.position = position
         self.years_of_service = years_of_service
@@ -50,44 +50,38 @@ def serialize_json(members: list, file_path: str) -> None:
     members_json = []
 
     for member in members:
-        if isinstance(member, Student):
-            member_json = {
-                "type": "student",
-                "value":{
+        member_json = {}
+        if isinstance(member, UniversityMember):
+            member_json["value"] = {
                     "id": member.id,
                     "name": member.name,
-                    "age": member.age,
+                    "age": member.age
+                }
+        else:
+            raise TypeError()
+        if isinstance(member, Student):
+            member_json["type"] = "student"
+            member_json["value"].update({
                     "course": member.course,
                     "year": member.year,
                     "gpa": member.gpa
-                }
-            }
+            })
             members_json.append(member_json)
         elif isinstance(member, Professor):
-            member_json = {
-                "type": "professor",
-                "value":{
-                    "id": member.id,
-                    "name": member.name,
-                    "age": member.age,
+            member_json["type"] = "professor"
+            member_json["value"].update({
                     "department": member.department,
                     "title": member.title,
                     "salary": member.salary
-                }
-            }
+            })
             members_json.append(member_json)
         elif isinstance(member, AdminStaff):
-            member_json = {
-                "type": "adminstaff",
-                "value":{
-                    "id": member.id,
-                    "name": member.name,
-                    "age": member.age,
+            member_json["type"] = "adminstaff"
+            member_json["value"].update({
                     "position": member.position,
                     "years_of_service": member.years_of_service,
                     "salary": member.salary
-                }
-            }
+            })
             members_json.append(member_json)
 
     with open(file_path, "w") as f:
@@ -106,40 +100,24 @@ def deserialize_json(file_path: str) -> list:
 
     members = []
 
-    try:
-        if os.path.exists(file_path):
-            with open(file_path) as f:
-                members_json = json.load(f)
-            for member in members_json:
-                value = member["value"]
-                if member["type"] == "student":
-                    member = Student(value["id"],
-                                    value["name"],
-                                    value["age"],
-                                    value["course"],
-                                    value["year"],
-                                    value["gpa"])
-                    members.append(member)
-                elif member["type"] == "professor":
-                    member = Professor(value["id"],
-                                    value["name"],
-                                    value["age"],
-                                    value["department"],
-                                    value["title"],
-                                    value["salary"])
-                    members.append(member)
-                elif member["type"] == "adminstaff":
-                    member = AdminStaff(value["id"],
-                                    value["name"],
-                                    value["age"],
-                                    value["position"],
-                                    value["years_of_service"],
-                                    value["salary"])
-                    members.append(member)
-                else:
-                    print(f"Unsupported type {member['type']} with value {member['value']}")
-    except KeyError as err:
-        pass
+    if not os.path.exists(file_path):
+        return members
+
+    with open(file_path) as f:
+        members_json = json.load(f)
+    for member in members_json:
+        value = member["value"]
+        if member["type"] == "student":
+            member = Student(**value)
+            members.append(member)
+        elif member["type"] == "professor":
+            member = Professor(**value)
+            members.append(member)
+        elif member["type"] == "adminstaff":
+            member = AdminStaff(**value)
+            members.append(member)
+        else:
+            print(f"Unsupported type {member['type']} with value {member['value']}")
     return members
 
 
@@ -148,12 +126,12 @@ def deserialize_pkl(file_path: str) -> list:
 
     members = []
 
-    try:
-        if os.path.exists(file_path):   
-            with open(file_path, 'rb') as f:
-                members = pickle.load(f)
-    except KeyError as err:
-        pass
+    if not os.path.exists(file_path):
+        return members
+
+    with open(file_path, 'rb') as f:
+        members = pickle.load(f)
+    
     return members
 
 
@@ -199,7 +177,7 @@ def remove_member_by_id(members: list) -> None:
         id_to_remove = int(input("\nEnter ID to remove: "))
         for member in members:
             if member.id == id_to_remove:
-                members.remove(member)
+                del member
                 print(f"\n{GREEN}ID {id_to_remove} successfully removed!{RESET}")
                 return
         print(f"\n{RED}ID {id_to_remove} not found.{RESET}")
@@ -280,19 +258,19 @@ def detect_file_format_by_parsing(file_path: str) -> str:
 def cli():
     """Launches the command line interface for managing."""
     
-    if detect_file_format_by_parsing(FILE) == "json":
-        members = deserialize_json(FILE)
-    elif detect_file_format_by_parsing(FILE) == "pickle":
-        members = deserialize_pkl(FILE)
+    if detect_file_format_by_parsing(FILE_NAME) == "json":
+        members = deserialize_json(FILE_NAME)
+    elif detect_file_format_by_parsing(FILE_NAME) == "pickle":
+        members = deserialize_pkl(FILE_NAME)
     else:
         print(f"{YELLOW}What file format to create:\n{RESET}")
         print(f"{YELLOW}1. JSON{RESET}")
         print(f"{YELLOW}2. PICKLE\n{RESET}")
         format = input(f"{BLUE}Please choose a format: {RESET}")
         if format == "1":
-            members = deserialize_json(FILE)
+            members = deserialize_json(FILE_NAME)
         elif format == "2":
-            members = deserialize_pkl(FILE)
+            members = deserialize_pkl(FILE_NAME)
 
     while True:
         print(f"{YELLOW}\nMenu:{RESET}")
@@ -321,9 +299,9 @@ def cli():
             print(f"{YELLOW}2. PICKLE\n{RESET}")
             format = input(f"{BLUE}Please choose a format: {RESET}")
             if format == "1":
-                serialize_json(members, FILE)
+                serialize_json(members, FILE_NAME)
             elif format == "2":
-                serialize_pkl(members, FILE)
+                serialize_pkl(members, FILE_NAME)
             break
         else:
             print(f"{RED}Invalid choice. Please select a valid option.{RESET}")
